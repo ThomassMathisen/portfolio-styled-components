094d90e6bc33cd96cfdb5004d188954ffdad7ca9
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-17 13:08:08 +0100

    Create navbar and intro components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import styled, { css } from "styled-components";
+import Intro from "./components/Intro";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <>
+      <Container>
+        <Navbar />
+        <Intro />
+      </Container>
+    </>
+  );
 }
 
 export default App;
