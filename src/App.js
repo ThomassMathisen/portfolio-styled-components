847e23f2c74d35ae600fdac38f1066ecdf7db00b
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-17 14:54:59 +0100

    Create service component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import styled, { css } from "styled-components";
 import Feature from "./components/Feature";
 import Intro from "./components/Intro";
 import Navbar from "./components/Navbar";
+import Service from "./components/Service";
 
 const Container = styled.div`
   height: 100vh;
@@ -9,15 +10,49 @@ const Container = styled.div`
   position: relative;
 `;
 
+const Shape = css`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const IntroShape = styled.div`
+  ${Shape}
+  clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
+  background-color: crimson;
+`;
+
+const FeatureShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 55% 0%, 33% 100%, 0% 100%);
+  background-color: crimson;
+`;
+
+const ServiceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 33% 0%, 33% 100%, 0% 100%);
+  background-color: crimson;
+`;
+
 function App() {
+  const smallScreen = window.screen.width <= 480 ? true : false;
   return (
     <>
       <Container>
         <Navbar />
         <Intro />
+        <IntroShape />
       </Container>
       <Container>
         <Feature />
+        <FeatureShape />
+      </Container>
+      <Container>
+        <Service />
+        {!smallScreen && <ServiceShape />}
       </Container>
     </>
   );
